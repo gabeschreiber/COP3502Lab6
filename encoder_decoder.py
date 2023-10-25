17f7cd315e388f8b4963fbdf7811088f2c30599c
gabeschreiber <schreiberg@ufl.edu>
2023-10-25 19:07:50 -0400

    finish main function

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -12,12 +12,21 @@ def encode(password):
         except ValueError:
                 encoded_password.clear()
                 raw_password = list(input('Please enter valid password: '))
-    print('Your password has been encoded and stored!')
+    print('Your password has been encoded and stored!\n')
     return encoded_password
 
 
 def main():
-    encoded_password = encode(input('Please enter your password to encode: '))
+    print('Menu\n' + ('-' * 13) + '\n1. Encode\n2. Decode\n3. Quit\n')
+    user_input = int(input('Please enter an option: '))
+    while user_input != 3:
+        if user_input == 1:
+            encoded_password = encode(input('Please enter your password to encode: '))
+        elif user_input == 2:
+            # FIXME add call to decode function
+            print()
+        print('Menu\n' + ('-' * 13) + '\n1. Encode\n2. Decode\n3. Quit\n')
+        user_input = int(input('Please enter an option: '))
 
 
 if __name__ == '__main__':
